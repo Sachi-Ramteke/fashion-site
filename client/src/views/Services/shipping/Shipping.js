fc83f51cdf077e8399b681219034311aedd542c1
Sachi-Ramteke <sachir418@gmail.com>
2024-01-01 19:45:14 +0530

    nth commit

--- client/src/views/Services/shipping/Shipping.js
+++ client/src/views/Services/shipping/Shipping.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Navbar from "../../../components/Navbar/navbar";
 import Sidebar from "../../../components/Sidebar/Sidebar";
-import { useState } from 'react';
+import { useState } from "react";
+import "./Shipping.css";
+import Footer from "../../../components/Footer/Footer";
 
 function Shipping() {
   const [sidebar, setSidebar] = useState(false);
@@ -11,9 +13,51 @@ function Shipping() {
   return (
     <>
       <Navbar openSidebar={toggleSidebar} closeSidebar={toggleSidebar} />
-      <div>
-        <Sidebar sidebar={sidebar} />
+      <Sidebar sidebar={sidebar} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div className="shipping-div">
+          <h2 style={{ textShadow: "1px 1px 1px black", textAlign: "center" }}>
+            SHIPPING POLICY
+          </h2>
+          <h3>Shipping/Processing Time</h3>
+          <p>
+            The orders are usually dispatched in 3-7 business days, but please
+            allow up to 2 weeks (3 weeks if it is during a sale). All packages
+            are shipped from our partnered fulfillment center in India, the
+            package should arrive within 7-15 days for the western countries and
+            other countries' shipping times vary from 7-21 days. Please allow
+            for 30 days for the item to arrive after being shipped because
+            packages can be stuck in customs since some countries are stricter
+            on customs.
+          </p>
+          <h3>Lost Packages</h3>
+          <p>
+            We will take responsibility for any lost packages lost within our
+            shipping country. Any packages that are lost outside of our shipping
+            country must be taken up with your courier/local post unless you
+            have purchased insured shipping. We can provide the contact
+            information and tracking. If the package does not exit our shipping
+            zone within 60 business days (because it was lost) after tracking
+            has been posted out we will provide a full refund or reshipment.
+          </p>
+          <h3>Shipping Delays</h3>
+          <p>
+            In the event a circumstance that is out of our control delays
+            shipping, we will mention this on our website and you as the buyer
+            understand these risks. Our company will provide our best estimates
+            as to how long the delays are but we cannot guarantee any delivery
+            dates during these circumstances. Upon shipping fulfillment of your
+            order, the ability to issue standard refunds will no longer be
+            available.
+          </p>
+        </div>
       </div>
+      <Footer />
     </>
   );
 }
--- client/src/views/Services/sizing/Sizinginfo.js
+++ client/src/views/Services/sizing/Sizinginfo.js
@@ -1,19 +1,44 @@
 import React from "react";
 import Navbar from "../../../components/Navbar/navbar";
 import Sidebar from "../../../components/Sidebar/Sidebar";
-import { useState } from 'react';
+import { useState } from "react";
+import "./Sizinginfo.css";
+import Footer from "../../../components/Footer/Footer";
+import img1 from "./img/img1.png";
+import img2 from "./img/img2.png";
+import img3 from "./img/img3.png";
+
 
 function Sizinginfo() {
   const [sidebar, setSidebar] = useState(false);
   const toggleSidebar = () => {
     setSidebar((prevState) => !prevState);
   };
+
   return (
     <>
       <Navbar openSidebar={toggleSidebar} closeSidebar={toggleSidebar} />
-      <div>
-        <Sidebar sidebar={sidebar} />
+      <Sidebar sidebar={sidebar} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div className="sizing-div">
+          <h2 style={{ textShadow: "1px 1px 1px black", textAlign: "center" }}>
+            SHIPPING POLICY
+          </h2>
+          <h3 className="margin-top">SIZING RECOMMENDATION DISTRIBUTION</h3>
+          <img src={img1} alt="" className="size-img" />
+          <h3 className="margin-top">ASIA FITTING SIZE CONVERSION ESTIMATES</h3>
+          <img src={img2} alt="" className="size-img2" />
+          <h3 className="margin-top">how to measure</h3>
+          <img src={img3} alt="" className="size-img3" />
+        
+        </div>
       </div>
+      <Footer />
     </>
   );
 }
